66572eb5a7d635bd19cb1bae91000074e5a0f7e8
MinionAttack <10078549+MinionAttack@users.noreply.github.com>
2021-12-17 23:41:49 +0100

    Changed log mode.

--- resources/properties.py
+++ resources/properties.py
@@ -8,7 +8,7 @@ BUCKET_NAME = ""
 
 LOGS_FOLDER = 'logs'
 # Available loggers: development, production
-LOGS_MODE = 'development'
+LOGS_MODE = 'production'
 # Available levels: CRITICAL = 50, ERROR = 40, WARNING = 30, INFO = 20, DEBUG = 10
 LOGS_LEVEL = 20
 
